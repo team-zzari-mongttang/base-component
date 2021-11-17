a68b26b3c3fa77906a11546c68401c85677f520a
Kim hyunseok <dorrdorr9311@gmail.com>
2021-11-17 15:17:35 +0900

    set: alias 설정

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,17 @@
-module.exports ={
-  babel : {
+const CracoAlias = require('craco-alias');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: '.',
+        tsConfigPath: 'tsconfig.paths.json'
+      }
+    }
+  ],
+  babel: {
     presets: ['@emotion/babel-preset-css-prop']
   }
-}
+};
