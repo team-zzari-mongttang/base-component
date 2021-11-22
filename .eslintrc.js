0b73581f8980613f2c7c6411059f844e3be34914
Alang Kim <rlaangh77@naver.com>
2021-11-22 18:38:24 +0900

    fix: typescript plugin 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   ],
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended'
     // "plugin:prettier/recommended",
   ],
